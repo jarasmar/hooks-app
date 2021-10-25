d7ae7c48424da65f6701eba42f69f3b390a2a3e4
Jara Santamaria Martinez <jarasmar@gmail.com>
2021-10-25 16:58:38 +0100

    Add useEffect hook

--- src/components/songList.js
+++ src/components/songList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NewSongForm from "./NewSongForm";
 
 const SongList = () => {
@@ -7,10 +7,20 @@ const SongList = () => {
     { title: "Memory gospel", id: 2 },
     { title: "This wild darkness", id: 3 },
   ]);
+
+  const [age, setAge] = useState(29);
+
   const addSong = (title) => {
     // ...songs keeps the current songs, and then adds the new one
     setSongs([...songs, { title: title, id: songs.length + 1 }]);
   };
+
+  //   Fires in every re-render if no final array (life cycle method substitute)
+  //   Fires every time data pointed in the array (songs) changes
+  useEffect(() => {
+    console.log("use effect", songs);
+  }, [songs]);
+
   return (
     <div className="song-list">
       <ul>
@@ -19,6 +29,7 @@ const SongList = () => {
         })}
       </ul>
       <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
     </div>
   );
 };
